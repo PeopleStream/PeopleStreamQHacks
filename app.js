96ea725e350590547a6b3999b20a0728382b5b91
Brendan Kolisnik <bkolisnik@gmail.com>
2017-02-03 23:19:56 -0500

    Page rendering

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var router = express.Router();
+var path = require('path');
+var dbService = require('/services/dbService');
+
+app.use(express.static(__dirname + '/View'));
+app.use(express.static(__dirname + '/Script'));
 
 app.get('/',function(req,res){
 	
-res.send('Hello World')
+res.sendFile(path.join('index.html'));
 
-})
+});	
 
 app.listen(3000,function(){
 	
 console.log('Example app listening on port 3000')
 
-})
+});
